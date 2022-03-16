61adf6fc1321ad566fdcc5df2cf9e23890597258
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-03-16 14:22:27 +0100

    Added snackbar notification

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,22 +1,25 @@
-import { Button, FormControl, TextField } from "@mui/material";
+import { Button, FormControl, FormGroup, TextField } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 export default function Register() {
-	const [formError, setFormError] = useState(false)
-	const [formErrorMsg, setFormErrorMsg] = useState("")
 	const navigate = useNavigate()
+	const { enqueueSnackbar } = useSnackbar();
 
 	const handleSubmit = async (e) => {
 		e.preventDefault()
-		setFormError(false)
 
 		const email = e.target.email.value
 		const password = e.target.password.value
+		const firstname = e.target.firstname.value
+		const lastname = e.target.lastname.value
 
 		if (!email || !password) {
-			setFormError(true)
-			setFormErrorMsg("Not all form input are filled in")
+			enqueueSnackbar(`Not all form inputs have been filled in`, {
+				variant: "error",
+				autoHideDuration: 2500,
+			});
 			return
 		}
 
@@ -25,25 +28,39 @@ export default function Register() {
 			headers: {
 				"Content-Type": "application/json"
 			},
-			body: JSON.stringify({email, password})
+			body: JSON.stringify({email, password, firstname, lastname})
 		})
+		
 		const result = await res.json()
+
 		if (res.status === 200) {
+			enqueueSnackbar(`Registration success`, {
+				variant: "success",
+				autoHideDuration: 2500,
+			});
 			console.log(result)
 			navigate("/login")
 		} else {
-			setFormError(true)
-			setFormErrorMsg(result.message)
+			enqueueSnackbar(`${result.message}`, {
+				variant: "error",
+				autoHideDuration: 2500,
+			});
 		}
 	}
 
 	return(
 		<>
 			<form onSubmit={handleSubmit} className="register-form">
-				{formError && <div>{formErrorMsg}</div>}
 				<FormControl>
-					<TextField label="Email" type="email" name="email" variant="standard" />
-					<TextField label="Password" type="password" name="password" variant="standard" />
+					<FormGroup >
+						<TextField label="Firstname" type="text" name="firstname" variant="standard" required/>
+						<TextField label="Lastname" type="text" name="lastname" variant="standard" required/>
+					</FormGroup>
+
+					<FormGroup >
+						<TextField label="Email" type="email" name="email" variant="standard" required/>
+						<TextField label="Password" type="password" name="password" variant="standard" required/>
+					</FormGroup>
 
 					<Button variant="contained" type="submit">
 						Register
--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import UserProvider from "./providers/UserProvider"
+import { SnackbarProvider } from "notistack";
+import Slide from "@mui/material/Slide";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<UserProvider>
-			<App />
-		</UserProvider>
+		<SnackbarProvider
+			anchorOrigin={{
+				vertical: "bottom",
+				horizontal: "left",
+			}}
+			maxSnack={1}
+			TransitionComponent={Slide}>
+			<UserProvider>
+				<App />
+			</UserProvider>
+		</SnackbarProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
