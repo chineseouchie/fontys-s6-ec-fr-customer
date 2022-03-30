ab0a9c7103404dc42e51cb3cd34f29458d9ff47c
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-03-30 12:33:54 +0200

    change route

--- src/components/Register.js
+++ src/components/Register.js
@@ -22,7 +22,7 @@ export default function Register() {
 			return
 		}
 
-		const res = await fetch("http://localhost:3001/api/v1/register", {
+		const res = await fetch("http://localhost:3001/api/v1/auth/register", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json"
