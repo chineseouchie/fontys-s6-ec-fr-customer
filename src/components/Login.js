4fe063ecf22aaa770bd22060a0e1e055a8bc2dc5
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-03-30 21:03:49 +0200

    test nginx

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,7 +23,7 @@ export default function Login() {
 		const password = e.target.password.value
 
 		try {
-			const res = await fetch("http://ec-auth/api/v1/auth/login", {
+			const res = await fetch("http://ec-auth:30001/api/v1/auth/login", {
 				method: "POST",
 				headers: {
 					"content-type": "application/json"
