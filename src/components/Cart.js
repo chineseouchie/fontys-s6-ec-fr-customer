6935a844daa15b2f57c4d51443ca232447cdfac6
Lau,Joey K.W <j.lau@student.fontys.nl>
2022-04-19 10:12:38 +0200

    Added place order

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,52 @@
 import { Box, Button } from "@mui/material"
+import { useSnackbar } from "notistack"
 import { useContext } from "react"
 import { CartContext } from "../providers/CartProvider"
+import { UserContext } from "../providers/UserProvider"
 
 export default function Cart() {
+	const ORDER_URL = process.env.REACT_APP_ORDER_URL
 	const { cart, removeItem } = useContext(CartContext)
+	const { user } = useContext(UserContext)
+	const { enqueueSnackbar } = useSnackbar();
 	console.log(cart)
+	console.log(user)
+
+	const onOrder = async () => {
+		const orderProducts = cart.map(e => ({product_uuid: e.product_uuid, quantity: e.quantity}))
+		
+		try {
+			const res = await fetch(ORDER_URL, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"Authorization": `jwt ${user.jwt}`
+				},
+				body: JSON.stringify({"orderProducts": orderProducts})
+			})
+
+			const result = await res.json()
+			if (res.status === 200) {
+				console.log(result)
+				enqueueSnackbar(`Order placed`, {
+					variant: "success",
+					autoHideDuration: 2500,
+				});
+			} else {
+				enqueueSnackbar(result.message, {
+					variant: "error",
+					autoHideDuration: 2500,
+				});
+			}
+	
+		} catch(e) {
+			console.log(e)
+			enqueueSnackbar(`Something went wrong. Try again later`, {
+				variant: "error",
+				autoHideDuration: 2500,
+			});
+		}
+	}
 
 	return(<>
 		{cart.map(item => (
@@ -14,7 +56,7 @@ export default function Cart() {
 			</Box>
 		))}
 		<Box>
-			<Button variant="contained">Order</Button>
+			<Button onClick={onOrder} variant="contained">Order</Button>
 		</Box>
 	</>)
 }
